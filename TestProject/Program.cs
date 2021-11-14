9e87bd8442dedaa4cf9ca15425a5cc693f7d0995
Erfan <dzhepparov.erfan@mail.ru>
2021-11-14 11:46:16 +0300

    Добавьте файлы проекта.

---
+++ TestProject/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectDataBase
+{
+    class Program
+    {
+        private static string connectionString = @"DATA SOURCE = ERFAN\SQLEXPRESSS; INITIAL CATALOG = MyDataBase;
+            INTEGRATED SECURITY = TRUE";
+        static void Main(string[] args)
+        {
+            DataTable dataTable = new DataTable();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sqlQuery = "SELECT ФИО FROM Person";
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(sqlQuery, connection))
+                    {
+                        adapter.Fill(dataTable);
+                        Console.WriteLine(dataTable.DefaultView.ToString());
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка соединения с БД!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
